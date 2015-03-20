9c10f82e1b274da3318f89794066371aee01cad3
Neil Vohra <nvohra@uga.edu>
2015-03-20 03:46:31 -0400

    Fixing more merge conflicts.

--- src/Driver.java
+++ src/Driver.java
@@ -3,8 +3,7 @@ import java.util.Scanner;
 
 public class Driver {
     public static void main(String[] args) throws FileNotFoundException {
-        double[][] array = Matrix.getHilbertArray(4);
-        Matrix matrix = new Matrix(array);
+        Matrix matrix = Matrix.getHilbertMatrix(4);
 
         // Decide how to round decimals in matrix; to what extent must error be calculated?
         System.out.println("Original matrix: \n" + matrix);
@@ -13,37 +12,32 @@ public class Driver {
         System.out.println("Matrix multiplied by itself: \n" + matrix.multiply(matrix));
         System.out.println("Matrix multiplied by scalar: \n" + matrix.multiply(10));
         System.out.println("Matrix transposed: \n" + matrix.transpose());
+        System.out.println("Matrix diagonalized: \n" + matrix.diagonalize());
+        System.out.println("Matrix absolute value: \n" + matrix.absoluteValue());
         // Dot product just calls multiply method, so not bothering to test it
 
         Matrix[] list = new Matrix[2];
+
         list = matrix.lu_fact();
         System.out.println("LU decomposition of matrix:");
         System.out.println("L\n" + list[0] + "\nU\n" + list[1]);
         System.out.println("To ensure that the LU decomposition is correct, L and U should "
                 + "multiply together to form the original matrix. Result of L * U is below.");
         System.out.println(list[0].multiply(list[1]));
-        System.out.println("HERE I AM + \n" + matrix);
 
         list = matrix.qr_fact_givens();
         System.out.println("QR factorization of matrix using Givens Rotations:");
         System.out.println("Q\n" + list[0] + "\nR\n" + list[1]);
         System.out.println("To ensure that the QR factorization is correct, Q and R should "
-                + "multiply together to form the original matrix. Result of L * U is below.");
+                + "multiply together to form the original matrix. Result of Q * R is below.");
         System.out.println(list[0].multiply(list[1]));
-
 
         list = matrix.qr_fact_househ();
         System.out.println("QR factorization of matrix using HH Rotations:");
         System.out.println("Q\n" + list[0] + "\nR\n" + list[1]);
         System.out.println("To ensure that the QR factorization is correct, Q and R should "
-                + "multiply together to form the original matrix. Result of L * U is below.");
+                + "multiply together to form the original matrix. Result of Q * R is below.");
         System.out.println(list[0].multiply(list[1]));
-
-
-
-
-        System.out.println("Matrix diagonalized: \n" + matrix.diagonalize());
-        System.out.println("Matrix absolute value: \n" + matrix.absoluteValue());
 
         // Testing FileParser below
         // Added data.dat file to project folder, so test with that if you need to
