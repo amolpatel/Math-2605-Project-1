c8e2fb2430e10c77fbb49ebcc4fccdf04264396e
BhavaniJaladanki <bjaladanki3@gatech.edu>
2015-03-29 15:07:12 -0400

    Updated

--- src/PowerMethod.java
+++ src/PowerMethod.java
@@ -19,58 +19,74 @@ public class PowerMethod extends Matrix {
 	
 	public void power_method() {
 		Matrix nextVector = new Matrix(this.getNumRows(), this.getNumCols());
+		Matrix tempVector = new Matrix(this.getNumRows(), this.getNumCols());
 		int i = 0;
-		double difference = 0;
+		double difference = 100;
 		double nextEigenValue = 0;
-		while(!(i > 100) || difference <= this.tol) {
+		while(i < 100 && difference > this.tol) {
+			//make tempVector 0
+			for (int x = 0; x < tempVector.getNumRows(); x++) {
+				tempVector.set(x,0,0);
+			}
 			//(A*u0) / eigen0 = u1
 			if (!(this.getNumRows() == matrixA.getNumCols())) {
 				throw new IllegalArgumentException("Matrices cannot be multiplied.");
 			}
-			double firstNum = this.get(1,0); //get first number of current
+			double firstNum = this.get(0,0); //get first number of current
 			for (int n = 0; n < matrixA.getNumRows(); n++) {
 				for (int j = 0; j < matrixA.getNumCols(); j++) {
-					for (int k = 0; k < this.getNumRows(); k++) {
-						this.set(n,0, (this.get(n,0) + matrixA.get(n,j) * this.get(k,0)));
-					}
+					tempVector.set(n,0, (tempVector.get(n,0) + matrixA.get(n,j) * this.get(j,0)));
 				}
 			}
 			//now divide each number in vector by previous eigenvalue
-			for (int j = 0; j < this.getNumRows(); i++) {
-				this.set(j, 0, this.get(j,0) / firstNum);
+			for (int j = 0; j < tempVector.getNumRows(); j++) {
+				tempVector.set(j, 0, (tempVector.get(j,0) / firstNum));
+			}
+			//this = tempVector
+			for (int x = 0; x < tempVector.getNumRows(); x++) {
+				this.set(x,0,(tempVector.get(x,0)));
 			}
 			
 			//check for error - we now have Ak; either get Ak-1 or Ak+1
 			if (!(this.getNumRows() == matrixA.getNumCols())) {
 				throw new IllegalArgumentException("Matrices cannot be multiplied.");
 			}
-			double firstNumm = this.get(1,0); //get first number of current
+		
+			//make tempVector 0
+			for (int x = 0; x < tempVector.getNumRows(); x++) {
+				tempVector.set(x,0,0);
+			}
+
+			double firstNumm = this.get(0,0); //get first number of current
 			for (int n = 0; n < matrixA.getNumRows(); n++) {
 				for (int j = 0; j < matrixA.getNumCols(); j++) {
-					for (int k = 0; k < this.getNumRows(); k++) {
-						nextVector.set(n, 0, (nextVector.get(n,0) + matrixA.get(n,j) * this.get(k,0)));
-					}
+					tempVector.set(n, 0, (tempVector.get(n,0) + matrixA.get(n,j) * this.get(j,0)));
 				}
 			}
 			//now divide each number in vector by previous eigenvalue
-			for (int j = 0; j < nextVector.getNumRows(); i++) { 
-				nextVector.set(j, 0, nextVector.get(j, 0) / firstNumm);
+			for (int j = 0; j < tempVector.getNumRows(); j++) { 
+				tempVector.set(j, 0, (tempVector.get(j, 0) / firstNumm));
 			} 
+			//nextVector = tempVector
+			for (int x = 0; x < tempVector.getNumRows(); x++) {
+				nextVector.set(x,0,(tempVector.get(x,0)));
+			}
+
 			//I now have next vector and current vector to check against each other
 
 			//perform check
-			double currEigenValue = this.get(1,0);
-			nextEigenValue = nextVector.get(1,0);
+			double currEigenValue = this.get(0,0);
+			nextEigenValue = nextVector.get(0,0);
 			double multiply;
 			difference = nextEigenValue - currEigenValue;
+			difference = difference/nextEigenValue;
 			if (difference < 0) {
 				multiply = difference * 2;
-				difference = difference - multiply;
+				difference = difference - multiply;		
 			}
 			i++;
-			
 		}
-		if (i > 100) {
+		if (i >= 100) {
 			System.out.println("Method does not converge after 100 iterations");
 		} else {
 			System.out.println("The maximum eigenvalue is " + nextEigenValue);
